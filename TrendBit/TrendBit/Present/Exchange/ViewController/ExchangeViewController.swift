20c63742ba2544bfe0b0c871277f3ac528baea1d
Minsoo Kang <alstn38@naver.com>
2025-03-06 19:41:40 +0900

    feat: ExchangeViewController 생성

---
+++ TrendBit/TrendBit/Present/Exchange/ViewController/ExchangeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ExchangeViewController.swift
+//  TrendBit
+//
+//  Created by 강민수 on 3/6/25.
+//
+
+import UIKit
+
+final class ExchangeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
