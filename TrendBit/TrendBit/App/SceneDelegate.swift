bd19c841e75c70cd3e471f44e78431937b98570f
Minsoo Kang <alstn38@naver.com>
2025-03-06 19:42:29 +0900

    chore: SceneDelegate 초기화면 수정

--- TrendBit/TrendBit/App/SceneDelegate.swift
+++ TrendBit/TrendBit/App/SceneDelegate.swift
@@ -13,9 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
-        let viewController = ViewController()
+        let tabBarController = TrendBitTabBarController()
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = viewController
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
 
