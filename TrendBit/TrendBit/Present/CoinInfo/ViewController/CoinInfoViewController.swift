003beb75a8f1c3b97fd8ab3dad0414fe267936bb
Minsoo Kang <alstn38@naver.com>
2025-03-06 19:41:32 +0900

    feat: CoinInfoViewController 생성

---
+++ TrendBit/TrendBit/Present/CoinInfo/ViewController/CoinInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CoinInfoViewController.swift
+//  TrendBit
+//
+//  Created by 강민수 on 3/6/25.
+//
+
+import UIKit
+
+final class CoinInfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
