780c62298d3af2fd515f9ba9bf1ec9b429e9b39f
Minsoo Kang <alstn38@naver.com>
2025-03-06 19:42:15 +0900

    feat: TrendBitTabBarController 구현

---
+++ TrendBit/TrendBit/Present/TrendBitTabBar/TrendBitTabBarController.swift
@@ -0,0 +1,64 @@
+//
+//  TrendBitTabBarController.swift
+//  TrendBit
+//
+//  Created by 강민수 on 3/6/25.
+//
+
+import UIKit
+
+final class TrendBitTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTabBar()
+    }
+    
+    private func configureTabBar() {
+        view.backgroundColor = UIColor(resource: .trendBitWhite)
+        configureTabBarController()
+        configureTabBarAppearance()
+    }
+    
+    private func configureTabBarController() {
+        let exchangeViewController = ExchangeViewController()
+        exchangeViewController.tabBarItem = UITabBarItem(
+            title: StringLiterals.TapBar.exchangeTitle,
+            image: ImageAssets.chartLineUptrendAxis,
+            selectedImage: ImageAssets.chartLineUptrendAxis
+        )
+        
+        let coinInfoViewController = CoinInfoViewController()
+        coinInfoViewController.tabBarItem = UITabBarItem(
+            title: StringLiterals.TapBar.coinInfoTitle,
+            image: ImageAssets.chartBarFill,
+            selectedImage: ImageAssets.chartBarFill
+        )
+        
+        let portfolioViewController = PortfolioViewController()
+        portfolioViewController.tabBarItem = UITabBarItem(
+            title: StringLiterals.TapBar.portfolioTitle,
+            image: ImageAssets.star,
+            selectedImage: ImageAssets.star
+        )
+        
+        let exchangeNavigationController = UINavigationController(rootViewController: exchangeViewController)
+        let coinInfoNavigationController = UINavigationController(rootViewController: coinInfoViewController)
+        let portfolioNavigationController = UINavigationController(rootViewController: portfolioViewController)
+        setViewControllers(
+            [exchangeNavigationController, coinInfoNavigationController, portfolioNavigationController],
+            animated: true
+        )
+    }
+    
+    private func configureTabBarAppearance() {
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBarAppearance.backgroundColor = UIColor(resource: .trendBitWhite)
+        tabBarAppearance.stackedLayoutAppearance.selected.iconColor = UIColor(resource: .trendBitNavy)
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+        tabBar.tintColor = UIColor(resource: .trendBitNavy)
+    }
+}
