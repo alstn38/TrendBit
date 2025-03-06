07e82a1eb8e1106531fc920b356e1c305aa5f1b7
Minsoo Kang <alstn38@naver.com>
2025-03-06 17:52:54 +0900

    init: TrendBit Project 생성

---
+++ TrendBit/TrendBit/Present/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendBit
+//
+//  Created by 강민수 on 3/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
