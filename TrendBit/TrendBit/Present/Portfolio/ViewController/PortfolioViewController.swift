59aa06112c574d59f7c4ab9a02edceccc21329d5
Minsoo Kang <alstn38@naver.com>
2025-03-06 19:41:53 +0900

    feat: PortfolioViewController 생성

---
+++ TrendBit/TrendBit/Present/Portfolio/ViewController/PortfolioViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PortfolioViewController.swift
+//  TrendBit
+//
+//  Created by 강민수 on 3/6/25.
+//
+
+import UIKit
+
+final class PortfolioViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
