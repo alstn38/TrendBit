41d972cffbabcef51b3a5e9955b648a23ab3ed73
Minsoo Kang <alstn38@naver.com>
2025-03-06 19:40:50 +0900

    chore: StringLiterals TabBar Title text 추가

---
+++ TrendBit/TrendBit/Resource/StringLiterals.swift
@@ -0,0 +1,21 @@
+//
+//  StringLiterals.swift
+//  TrendBit
+//
+//  Created by 강민수 on 3/6/25.
+//
+
+import Foundation
+
+enum StringLiterals {
+    
+    enum NavigationItem {
+        static let backButtonTitle: String = ""
+    }
+    
+    enum TapBar {
+        static let exchangeTitle: String = "거래소"
+        static let coinInfoTitle: String = "코인정보"
+        static let portfolioTitle: String = "포트폴리오"
+    }
+}
