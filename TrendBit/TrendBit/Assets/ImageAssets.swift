48760ad4497dc60e96068c6e21ff307da4cd329f
Minsoo Kang <alstn38@naver.com>
2025-03-06 18:20:28 +0900

    setting: Assets Image 추가

---
+++ TrendBit/TrendBit/Assets/ImageAssets.swift
@@ -0,0 +1,20 @@
+//
+//  ImageAssets.swift
+//  TrendBit
+//
+//  Created by 강민수 on 3/6/25.
+//
+
+import UIKit
+
+enum ImageAssets {
+    static let arrowTriangleUpFill = UIImage(systemName: "arrowtriangle.up.fill")
+    static let arrowTriangleDownFill = UIImage(systemName: "arrowtriangle.down.fill")
+    static let magnifyingGlass = UIImage(systemName: "magnifyingglass")
+    static let starFill = UIImage(systemName: "star.fill")
+    static let star = UIImage(systemName: "star")
+    static let chevronRight = UIImage(systemName: "chevron.right")
+    static let arrowLeft = UIImage(systemName: "arrow.left")
+    static let chartBarFill = UIImage(systemName: "chart.bar.fill")
+    static let chartLineUptrendAxis = UIImage(systemName: "chart.line.uptrend.xyaxis")
+}
