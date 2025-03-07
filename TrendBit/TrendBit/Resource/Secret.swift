0659b1fd066b02df1c9525ea026a660bce842340
Minsoo Kang <alstn38@naver.com>
2025-03-08 01:45:53 +0900

    featL Secret BaseURL 정의

---
+++ TrendBit/TrendBit/Resource/Secret.swift
@@ -0,0 +1,27 @@
+//
+//  Secret.swift
+//  TrendBit
+//
+//  Created by 강민수 on 3/7/25.
+//
+
+import Foundation
+
+enum Secret {
+    
+    static let upbitBaseURL: String = {
+        guard let urlString = Bundle.main.infoDictionary?["UPBIT_BASE_URL"] as? String else {
+            fatalError("UPBIT_BASE_URL ERROR")
+        }
+        
+        return urlString
+    }()
+    
+    static let coingeckoBaseURL: String = {
+        guard let urlString = Bundle.main.infoDictionary?["COINGECKO_BASE_URL"] as? String else {
+            fatalError("COINGECKO_BASE_URL ERROR")
+        }
+        
+        return urlString
+    }()
+}
