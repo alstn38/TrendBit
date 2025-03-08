0408227325a5353410a37f9f43e722e0442f6b80
Minsoo Kang <alstn38@naver.com>
2025-03-09 03:13:28 +0900

    feat: ExchangeTableViewCell Configure Data method 추가

--- TrendBit/TrendBit/Present/Exchange/Cell/ExchangeTableViewCell.swift
+++ TrendBit/TrendBit/Present/Exchange/Cell/ExchangeTableViewCell.swift
@@ -29,29 +29,43 @@ final class ExchangeTableViewCell: UITableViewCell, ReusableViewProtocol {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func configureCell(with data: ExchangeDataEntity) {
+        coinNameLabel.text = data.coinName
+        currentPriceLabel.text = data.currentPrice
+        previousDayPercentLabel.text = data.changePercent
+        previousDayAmountLabel.text = data.changePrice
+        transactionAmount.text = data.transactionAmount
+        
+        switch data.changeState {
+        case .rise:
+            previousDayPercentLabel.textColor = UIColor(resource: .trendBitRed)
+            previousDayAmountLabel.textColor = UIColor(resource: .trendBitRed)
+        case .fall:
+            previousDayPercentLabel.textColor = UIColor(resource: .trendBitBlue)
+            previousDayAmountLabel.textColor = UIColor(resource: .trendBitBlue)
+        case .even:
+            previousDayPercentLabel.textColor = UIColor(resource: .trendBitGray)
+            previousDayAmountLabel.textColor = UIColor(resource: .trendBitGray)
+        }
+    }
+    
     private func configureView() {
         contentView.backgroundColor = UIColor(resource: .trendBitWhite)
         
-        coinNameLabel.text = "기본코인" // TODO: 서버 연결 시 삭제
         coinNameLabel.textColor = UIColor(resource: .trendBitNavy)
         coinNameLabel.font = .systemFont(ofSize: 12, weight: .bold)
         coinNameLabel.numberOfLines = 1
         
-        currentPriceLabel.text = "4,025,000" // TODO: 서버 연결 시 삭제
         currentPriceLabel.textColor = UIColor(resource: .trendBitGray)
         currentPriceLabel.font = .systemFont(ofSize: 12, weight: .regular)
         currentPriceLabel.numberOfLines = 1
         
-        previousDayPercentLabel.text = "-3.72%" // TODO: 서버 연결 시 삭제
         previousDayPercentLabel.font = .systemFont(ofSize: 12, weight: .regular)
         previousDayPercentLabel.numberOfLines = 1
         
-        
-        previousDayAmountLabel.text = "-154" // TODO: 서버 연결 시 삭제
         previousDayAmountLabel.font = .systemFont(ofSize: 9, weight: .regular)
         previousDayAmountLabel.numberOfLines = 1
         
-        transactionAmount.text = "180,826백만" // TODO: 서버 연결 시 삭제
         transactionAmount.textColor = UIColor(resource: .trendBitGray)
         transactionAmount.font = .systemFont(ofSize: 12, weight: .regular)
         transactionAmount.numberOfLines = 1
