89233f57eb7b2195215c2d8a2d099ccc9cf2c90a
Minsoo Kang <alstn38@naver.com>
2025-03-10 20:10:22 +0900

    fix: DateFormatter ISO8601DateFormatter 속성 수정

--- TrendBit/TrendBit/Model/Formatter/DateFormatterManager.swift
+++ TrendBit/TrendBit/Model/Formatter/DateFormatterManager.swift
@@ -12,6 +12,7 @@ final class DateFormatterManager {
     private let trendDateFormatter = DateFormatter()
     private let detailUpdateDateFormatter = DateFormatter()
     private let detailAllTheTimeDateFormatter = DateFormatter()
+    private let serverDateFormatter = ISO8601DateFormatter()
     
     static let shared = DateFormatterManager()
     
@@ -24,6 +25,8 @@ final class DateFormatterManager {
         
         detailAllTheTimeDateFormatter.locale = Locale(identifier: "ko_KR")
         detailAllTheTimeDateFormatter.dateFormat = "yy년 M월 d일"
+        
+        serverDateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
     }
     
     /// Trend Info Update 시간을 "MM.dd HH:mm 기준" 형식으로 반환하는 메서드
@@ -38,7 +41,7 @@ final class DateFormatterManager {
     
     /// Detail 역대 최고/최저 시간을 ""yy년 M월 d일"" 형식으로 반환하는 메서드
     func detailAllTheTimeString(dateString: String) -> String {
-        guard let isoDate = ISO8601DateFormatter().date(from: dateString) else {
+        guard let isoDate = serverDateFormatter.date(from: dateString) else {
             return "시간 정보 없음"
         }
         return detailAllTheTimeDateFormatter.string(from: isoDate)
