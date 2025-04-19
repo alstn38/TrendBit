21182306d27a2fe851dad7328193c91cf12b5a69
Minsoo Kang <alstn38@naver.com>
2025-04-19 19:18:52 +0900

    feat: Socket Base URL 추가

--- TrendBit/TrendBit/Resource/Secret.swift
+++ TrendBit/TrendBit/Resource/Secret.swift
@@ -17,6 +17,14 @@ enum Secret {
         return urlString
     }()
     
+    static let upbitSoketURL: String = {
+        guard let urlString = Bundle.main.infoDictionary?["UPBIT_SOKET_URL"] as? String else {
+            fatalError("UPBIT_SOKET_URL ERROR")
+        }
+        
+        return urlString
+    }()
+    
     static let coingeckoBaseURL: String = {
         guard let urlString = Bundle.main.infoDictionary?["COINGECKO_BASE_URL"] as? String else {
             fatalError("COINGECKO_BASE_URL ERROR")
